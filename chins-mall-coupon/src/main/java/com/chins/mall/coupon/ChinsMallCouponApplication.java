4b72dcc6622cda66e894fef1517a73c664e0db51
chintensakai <toy_shen@163.com>
2020-08-02 22:45:37 +0800

    注册服务到注册中心

--- chins-mall-coupon/src/main/java/com/chins/mall/coupon/ChinsMallCouponApplication.java
+++ chins-mall-coupon/src/main/java/com/chins/mall/coupon/ChinsMallCouponApplication.java
@@ -1,9 +1,13 @@
 package com.chins.mall.coupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.chins.mall.coupon.mapper")
 public class ChinsMallCouponApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChinsMallCouponApplication.class, args);
